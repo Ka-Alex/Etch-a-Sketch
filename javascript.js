ed5cb943657b6ded2733b9cf3036077d88ee2bb2
KA <191128298+KarlAlexander@users.noreply.github.com>
2025-02-15 15:46:49 +0100

    Add the drawing feature
    Hovering the grid changes the background color of items

--- javascript.js
+++ javascript.js
@@ -1,5 +1,9 @@
 const container = document.querySelector('#container');
 
+const changeColor = (e) => {
+    e.target.style.background = '#000000';
+}
+
 //Loop to create successive rows
 for (let i = 0; i < 16; i++) {
     let row = document.createElement('div');
@@ -12,6 +16,8 @@ for (let i = 0; i < 16; i++) {
         let item = document.createElement('div');
         item.setAttribute('id', 'item'+j);
         item.classList.add('item', 'item');
+        item.addEventListener('mouseover', changeColor);
         row.appendChild(item);
     }
 }
+
