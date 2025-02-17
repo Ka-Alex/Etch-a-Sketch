1af7a0d7323a8de519ba797e4f7ef774f8e2582f
KA <191128298+KarlAlexander@users.noreply.github.com>
2025-02-17 16:42:51 +0100

    Add the feature to create a new grid
    The user can now decide the number of squares per side for the new grid

--- javascript.js
+++ javascript.js
@@ -1,23 +1,52 @@
-const container = document.querySelector('#container');
+'use strict'
+let userInput = 16;
+
+let container = document.querySelector('#container');
+
 
 const changeColor = (e) => {
-    e.target.style.background = '#000000';
-}
+    
+    console.log(e);
+    
+    e.target.style.backgroundColor = "rgb("+Math.round(Math.random()*255)+","+Math.round(Math.random()*255)+","+Math.round(Math.random()*255)+")";
+}   
 
-//Loop to create successive rows
-for (let i = 0; i < 16; i++) {
+createGrid();
+
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', () => {
+   
+    do {
+        userInput = parseInt(prompt('Enter the size of the grid (between 2 and 100)'));
+        console.log(userInput);
+        
+    } while ((isNaN(userInput)) || (userInput > 100 || userInput < 2))
+
+    container.remove();
+    container = document.createElement('div');
+    container.setAttribute('id', 'container');
+    document.body.appendChild(container);
+    createGrid(userInput);
+});
+
+function createGrid(userInput = 16) {
+    
+    //Loop to create successive rows
+for (let i = 0; i < userInput; i++) {
     let row = document.createElement('div');
     row.setAttribute('id', 'row'+i);
     row.classList.add('row');
     container.appendChild(row);
 
     //Loop to create suucessive items
-    for (let j = 0; j < 16; j++) {
+    for (let j = 0; j < userInput; j++) {
         let item = document.createElement('div');
         item.setAttribute('id', 'item'+j);
         item.classList.add('item', 'item');
         item.addEventListener('mouseover', changeColor);
         row.appendChild(item);
     }
+}
 }
 
